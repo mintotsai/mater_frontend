7f1911bd606369609312f9ddaa5d9d1942c92e56
Minto Tsai <mintotsai@jasminepm.com>
2022-01-03 09:03:04 -0600

    Use Formik and yup

--- src/components/Authentication/Signup.jsx
+++ src/components/Authentication/Signup.jsx
@@ -1,73 +1,16 @@
-/*
-  This example requires Tailwind CSS v2.0+
-
-  This example requires some changes to your config:
-
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-import React, { useState } from 'react';
-import { LockClosedIcon } from '@heroicons/react/solid'
+import React from 'react';
 import authenticationApi from '../../apis/authentication';
 import { useAuthDispatch } from '../../contexts/auth';
 import { setAuthHeaders, resetAuthTokens } from '../../apis/axios';
 
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
 const Signup = () => {
-  const [initialValues, setInitialValues] = useState({
-    email: '',
-    password: '',
-    passwordConfirmation: '',
-  });
-
-  const [loading, setLoading] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirmation, setConfirmationPassword] = useState('');
   const authDispatch = useAuthDispatch();
-
-  const handleSubmitExternally = async e => {
-    e.preventDefault()
-    // console.log('>>>');
-    // const { email, password } = values;
-    // console.log(">>>" + email + " " + password);
-    try {
-      setLoading(true);
-      const response = await authenticationApi.signup({ user: { email, password, password_confirmation: passwordConfirmation, } });
-      console.log(">>>");
-      console.log(response);
-      let auth_token;
-      if (response.headers.authorization && response.headers.authorization.split(' ')[0] === 'Bearer') {
-        auth_token = response.headers.authorization.split(' ')[1];
-      }
-      authDispatch({ type: 'LOGIN', payload: { auth_token, email } });
-      // userDispatch({ type: 'SET_USER', payload: { user } });
-      resetAuthTokens();
-      setAuthHeaders();
-    } catch (error) {
-
-    } finally {
-      setLoading(false);
-    }
-  }
 
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-50">
-        <body class="h-full">
-        ```
-      */}
       <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <img
@@ -80,69 +23,120 @@ const Signup = () => {
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6">
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                  Email address
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-              </div>
+            <Formik
+              initialValues={{
+                email: '',
+                password: '',
+                passwordConfirmation: '',
+              }}
+              validationSchema={Yup.object().shape({
+                email: Yup.string().email('Invalid email').required('Required'),
+                password: Yup.string()
+                  .min(2, 'Too Short!')
+                  .max(50, 'Too Long!')
+                  .required('Required'),
+                passwordConfirmation: Yup.string()
+                  .min(2, 'Too Short!')
+                  .max(50, 'Too Long!')
+                  .required('Required')
+                  .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+              })}
+              onSubmit={async (values, { setStatus, resetForm }) => {
+                // console.log(values);
+                resetForm();
+                try {
+                  const response = await authenticationApi.signup({ user: { email: values["email"], password: values["password"], password_confirmation: values["passwordConfirmation"] } });
+                  setStatus({
+                    success: true,
+                    msg: "Check your email to confirm your account."
+                  })
+                } catch (error) {
+                  console.log(">>>");
+                  console.log(error.response.data);
+                  // TODO: Is this the best? Test with existing email address in system"
+                  setStatus({
+                    success: false,
+                    msg: error.response.data.error
+                  })
+                } finally {
 
-              <div>
-                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                  Password
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-              </div>
+                }
+              }}
+            >
+              {({ errors, touched, isSubmitting, status }) => (
+                <Form className="space-y-6">
+                  {status && status.msg && (
+                    <div className={`rounded-md ${!status.success ? "bg-red-50" : "bg-green-50"} p-4`}>
+                      <div className="flex">
+                        <div className="ml-3">
+                          <h3 className={`text-sm font-medium ${!status.success ? "text-red-800" : "text-green-800"}`}>{status.msg}</h3>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                  <div>
+                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                      Email address
+                    </label>
+                    <div className="mt-1">
+                      <Field
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        required
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                    <ErrorMessage component="p" name="email" className="mt-2 text-sm text-red-600" />
+                  </div>
 
-              <div>
-                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                  Password Confirmation
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="password-confirmation"
-                    name="password-confirmation"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    onChange={(e) => setConfirmationPassword(e.target.value)}
-                  />
-                </div>
-              </div>
+                  <div>
+                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                      Password
+                    </label>
+                    <div className="mt-1">
+                      <Field
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        required
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                    <ErrorMessage component="p" name="password" className="mt-2 text-sm text-red-600" />
+                  </div>
 
-              <div>
-                <button
-                  type="submit"
-                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  onClick={handleSubmitExternally}
-                >
-                  Sign up
-                </button>
-              </div>
-            </form>
+                  <div>
+                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                      Password Confirmation
+                    </label>
+                    <div className="mt-1">
+                      <Field
+                        id="passwordConfirmation"
+                        name="passwordConfirmation"
+                        type="password"
+                        autoComplete="current-password"
+                        required
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                    <ErrorMessage component="p" name="passwordConfirmation" className="mt-2 text-sm text-red-600" />
+                  </div>
 
+                  <div>
+                    <button
+                      type="submit"
+                      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      disabled={isSubmitting}
+                    >
+                      Sign up
+                    </button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
           </div>
         </div>
       </div>
