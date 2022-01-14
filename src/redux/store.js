150ef4473f8248d6acfa757c27d3bcd9653a33a6
Minto Tsai <mintotsai@jasminepm.com>
2022-01-14 10:51:58 -0600

    Add thunk middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 // https://medium.com/@montezsmith/how-to-setup-redux-react-router-571adfde4f23
 // https://github.com/gothinkster/react-redux-realworld-example-app/blob/9186292054dc37567e707602a15a0884d6bdae35/src/store.js
 import { createStore, applyMiddleware } from 'redux'
-import logger from 'redux-logger'
+import createLogger from 'redux-logger'
+import thunkMiddleware from 'redux-thunk';
 // import { composeWithDevTools } from 'redux-devtools-extension'
 // import thunk from 'redux-thunk'
 import rootReducer from './reducers';
@@ -11,10 +12,10 @@ import rootReducer from './reducers';
 
 const getMiddleware = () => {
   if (process.env.NODE_ENV === 'production') {
-    return applyMiddleware();
+    return applyMiddleware(thunkMiddleware);
   } else {
     // Enable additional logging in non-production environments.
-    return applyMiddleware(logger)
+    return applyMiddleware(thunkMiddleware, createLogger)
   }
 };
 
