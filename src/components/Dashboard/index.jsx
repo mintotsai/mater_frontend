c8bf90189fde0fb74fbc661d5584fc43df88513b
Minto Tsai <mintotsai@jasminepm.com>
2022-01-23 08:34:12 -0600

    Add back logout

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -281,6 +281,7 @@ const Home = () => {
                                 active ? 'bg-gray-100' : '',
                                 'block px-4 py-2 text-sm text-gray-700'
                               )}
+                              onClick={onClick}
                             >
                               {item.name}
                             </a>
