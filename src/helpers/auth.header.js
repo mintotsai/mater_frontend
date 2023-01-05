0e7730ceae84288fb336e3af3b4d94f84fb94059
Minto Tsai <mintotsai@jasminepm.com>
2023-01-05 07:34:15 -0600

    Fix getting token from localstorage

--- src/helpers/auth.header.js
+++ src/helpers/auth.header.js
@@ -1,6 +1,7 @@
 export function authHeader() {
   // return authorization header with jwt token
-  const token = JSON.parse(localStorage.getItem('authToken'));
+  // const token = JSON.parse(localStorage.getItem('authToken'));
+  const token = localStorage.getItem('authToken');
 
   if (token) {
     return { 'Authorization': 'Bearer ' + token };
