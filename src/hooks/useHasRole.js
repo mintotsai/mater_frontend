9dfb494879b053890c18e189924f37940e866d2f
Minto Tsai <mintotsai@jasminepm.com>
2023-03-22 09:19:23 -0500

    Throw not implemented error for unused incorrect code

--- src/hooks/useHasRole.js
+++ src/hooks/useHasRole.js
@@ -1,5 +1,5 @@
 // https://blog.kiprosh.com/architect-react-apps-using-hooks/
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 // import useCurrentUser from "./useCurrentUser";
 
 export const useHasRole = (roleNames) => {
@@ -16,7 +16,8 @@ export const useHasRole = (roleNames) => {
   if (typeof roleNames === "string") {
     return userRoles.hasOwnProperty(roleNames) && userRoles[roleNames];
   } else if (Array.isArray(roleNames)) {
-    return roleNames.some((role) => role === userRoles);
+    // return roleNames.some((role) => role === userRoles); // TODO: This is wrong
+    throw new Error("Not Implemented")
   } else {
     return false;
   }
