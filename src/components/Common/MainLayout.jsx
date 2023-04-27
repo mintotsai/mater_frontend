a4a82091956fdd2e368f97b63db2bc157ec5fc17
Minto Tsai <mintotsai@jasminepm.com>
2023-04-27 15:16:18 -0500

    Comment out unused side menu items

--- src/components/Common/MainLayout.jsx
+++ src/components/Common/MainLayout.jsx
@@ -30,11 +30,7 @@ import ToastNotifications from "./ToastNotifications";
 */
 import { Dialog, Menu, Transition } from '@headlessui/react';
 import {
-  CalendarIcon,
-  ChartBarIcon,
-  FolderIcon,
   HomeIcon,
-  InboxIcon,
   MenuAlt2Icon,
   UsersIcon,
   XIcon
@@ -92,11 +88,11 @@ export default function MainLayout() {
   let navigation = [
     { name: 'Dashboard', href: '/home', icon: HomeIcon, current: true },
     { name: 'Patients', href: '/provider/patients', icon: UsersIcon, current: false },
-    { name: 'Team', href: '#', icon: UsersIcon, current: false },
-    { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-    { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-    { name: 'Documents', href: '#', icon: InboxIcon, current: false },
-    { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
+    // { name: 'Team', href: '#', icon: UsersIcon, current: false },
+    // { name: 'Projects', href: '#', icon: FolderIcon, current: false },
+    // { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
+    // { name: 'Documents', href: '#', icon: InboxIcon, current: false },
+    // { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
   ];
 
   if (isAdministrator) {
